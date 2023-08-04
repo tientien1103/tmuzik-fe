ca2c0811d61fe9599a0e54dee945d44f980110e1
Tien Le <124553193+tientien1103@users.noreply.github.com>
2023-08-04 20:22:25 +0700

    Update SongBar.js

--- src/components/SongBar.js
+++ src/components/SongBar.js
@@ -28,9 +28,11 @@ const SongBar = ({
         <Link to={`/songs/${song._id}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
-          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
-        </Link>
+       {!artistId && (
+          <Link to={`/artists/${song?.artists[0].adamid}`}>
+            <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+          </Link>
+        )}
       </div>
     </div>
 
