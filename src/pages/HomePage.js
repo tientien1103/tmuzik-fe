5094e142f8505cee570f63136fb66bf0ed6edd5b
Tien Le <124553193+tientien1103@users.noreply.github.com>
2023-08-08 17:25:08 +0700

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -24,6 +24,7 @@ const HomePage = () => {
       window.innerHeight + document.documentElement.scrollTop + 1 >=
       document.documentElement.scrollHeight
     ) {
+      setLoading(true);
       setPage((prev) => prev + 1);
     }
   };
